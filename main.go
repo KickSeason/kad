878f6d9d80d445610b7a01c7cab7e31f5b09bf4c
zhangtao <ztgoforit@163.com>
2018-12-28 18:30:17 +0800

    add find cmd

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"os"
 	"strings"
 
 	"github.com/KickSeason/kad/config"
@@ -12,16 +13,8 @@ import (
 	"github.com/c-bata/go-prompt"
 )
 
-var running = true
+var kb *kbs.KBS
 
-func interactor(d prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{
-		{Text: "table", Description: "get hash table"},
-		{Text: "store", Description: "store key-value into network"},
-		{Text: "exit", Description: "exit"},
-	}
-	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
-}
 func main() {
 	fmt.Println("nodeid: " + string(config.NodeID.String()))
 	ip := net.ParseIP(config.Address)
@@ -31,7 +24,7 @@ func main() {
 		Port:    config.Port,
 		ID:      config.NodeID,
 	}
-	kb := kbs.NewKBS(c)
+	kb = kbs.NewKBS(c)
 	s := server.Config{
 		IP:    ip,
 		Port:  config.Port,
@@ -43,29 +36,53 @@ func main() {
 	kb.Start()
 	fmt.Println("my kademlia impl")
 	go kb.Find(config.NodeID)
-	for running {
-		t := prompt.Input("> ", interactor)
-		switch t {
-		case "exit":
-			running = false
-		case "table":
-			fmt.Println(kb.ToJson())
-		default:
-			inputs := strings.Split(t, " ")
-			if inputs[0] == "store" {
-				if 3 == len(inputs) && inputs[1] != "" && inputs[2] != "" {
-					kb.Store(inputs[1], inputs[2])
+	pmt := prompt.New(executor, completer, prompt.OptionPrefixTextColor(prompt.Green),
+		prompt.OptionPrefix("kad>"))
+	pmt.Run()
+}
+
+func executor(input string) {
+	switch input {
+	case "exit":
+		os.Exit(0)
+	case "table":
+		fmt.Println(kb.ToJson())
+	default:
+		inputs := strings.Split(input, " ")
+		if 3 == len(inputs) && inputs[0] == "store" {
+			if inputs[1] != "" && inputs[2] != "" {
+				kb.Store(inputs[1], inputs[2])
+			}
+		} else if 2 == len(inputs) && inputs[0] == "get" {
+			if 2 == len(inputs) && inputs[1] != "" {
+				value, err := kb.Get(inputs[1])
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(value)
 				}
-			} else if inputs[0] == "get" {
-				if 2 == len(inputs) && inputs[1] != "" {
-					value, err := kb.Get(inputs[1])
-					if err != nil {
-						fmt.Println(err)
-					} else {
-						fmt.Println(value)
-					}
+			}
+		} else if 2 == len(inputs) && inputs[0] == "find" {
+			nid, err := kbs.NewIDFromString(inputs[1])
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				node, err := kb.Find(nid)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(node.ToJson())
 				}
 			}
 		}
 	}
+}
+
+func completer(doc prompt.Document) []prompt.Suggest {
+	s := []prompt.Suggest{
+		{Text: "table", Description: "get hash table"},
+		{Text: "store", Description: "store key-value into network"},
+		{Text: "exit", Description: "exit"},
+	}
+	return prompt.FilterHasPrefix(s, doc.GetWordBeforeCursor(), true)
 }
